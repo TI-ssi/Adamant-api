c4a9464a05ecd7d45b813f7d7b3747f59c261660
Patrick G. Leblanc <p.gleblanc@ti-ssi.com>
2020-01-26 02:22:51 +0000

    fixed table name

--- database/migrations/2016_01_26_020504_create_user_table.php
+++ database/migrations/2016_01_26_020504_create_user_table.php
@@ -13,7 +13,7 @@ class CreateUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('user', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
 	    $table->string('username');
 	    $table->string('password');
@@ -28,6 +28,6 @@ class CreateUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user');
+        Schema::dropIfExists('users');
     }
 }
