dc15b5bf26f1864734316a43c2259a645ebf6c68
Patrick G. Leblanc <p.gleblanc@ti-ssi.com>
2020-10-26 23:57:55 +0000

    update default for database

--- database/migrations/2018_11_27_185535_add_nickname_user.php
+++ database/migrations/2018_11_27_185535_add_nickname_user.php
@@ -14,7 +14,7 @@ class AddNicknameUser extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('nickname');
+            $table->string('nickname')->nullable();
         });
     }
 
